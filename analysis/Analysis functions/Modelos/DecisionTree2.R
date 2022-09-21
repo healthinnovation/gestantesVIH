400ebe281a48b199485fda5e47955590ddc81fcf
VanessaAguilar <vaauccahuaqui@gmail.com>
2022-09-20 21:09:53 -0500

    update

--- analysis/Analysis functions/Modelos/DecisionTree2.R
+++ analysis/Analysis functions/Modelos/DecisionTree2.R
@@ -6,26 +6,20 @@ test <- read.csv("./DataFinal/Train-TestSeparado/test.csv")
 glimpse(train)
 glimpse(test)
 
-
 table(train$CHECKUP_RULE_OUT_HIV)*100/nrow(train)
 table(test$CHECKUP_RULE_OUT_HIV)*100/nrow(test)
-library(reshape)
 
-df$CHECKUP_RULE_OUT_HIV <- as.factor(df$CHECKUP_RULE_OUT_HIV)
-glimpse(df)
+df_train <- train %>% select(-c(1,2))
+glimpse(df_train)
+df_test <- test %>% select(-c(1,2))
+glimpse(df_test)
 
-df$CHECKUP_RULE_OUT_HIV <- recode_factor(df$CHECKUP_RULE_OUT_HIV, '1' = '0', '0' = '1')
-table(df$CHECKUP_RULE_OUT_HIV)*100/nrow(df)
-
+df_train$CHECKUP_RULE_OUT_HIV <- as.factor(df_train$CHECKUP_RULE_OUT_HIV)
+df_test$CHECKUP_RULE_OUT_HIV <- as.factor(df_test$CHECKUP_RULE_OUT_HIV)
 #-------------------------------------
 
 set.seed(123)
-trees_split <- initial_split(df, strata = CHECKUP_RULE_OUT_HIV,prop = 0.80)
-trees_train <- training(trees_split)
-trees_test <- testing(trees_split)
-
-set.seed(123)
-trees_folds <- vfold_cv(trees_train, v = 5, strata = CHECKUP_RULE_OUT_HIV)
+trees_folds <- vfold_cv(df_train, v = 5, strata = CHECKUP_RULE_OUT_HIV)
 trees_folds
 
 trees_spec <- decision_tree(
@@ -36,13 +30,15 @@ trees_spec <- decision_tree(
   set_engine("rpart") %>%
   set_mode("classification")
 
-trees_rec <- recipe(CHECKUP_RULE_OUT_HIV~., data = trees_train)
+trees_rec <- recipe(CHECKUP_RULE_OUT_HIV~., data = df_train)
 
 trees_wf <- workflow() %>%
   add_model(trees_spec) %>%
   add_recipe(trees_rec)
 
-doParallel::registerDoParallel()
+library(doParallel)
+library(parallel)
+registerDoParallel()
 
 set.seed(123)
 trees_res <- tune_grid(
@@ -68,8 +64,8 @@ trees_res %>%
   labs(x = NULL, y = "J_Index")
 
 trees_grid <- grid_regular(
-  min_n(range = c(20,40)),
-  tree_depth(range = c(12, 16)),
+  min_n(range = c(15,30)),
+  tree_depth(range = c(4, 10)),
   cost_complexity(range = c(-8,-3),trans = log10_trans()) ,
   levels = 5
 )
@@ -97,6 +93,10 @@ show_best(regular_res,"specificity")
 show_best(regular_res,"j_index")
 
 best_auc <- select_best(regular_res, "j_index")
+"# A tibble: 1 x 4
+  cost_complexity tree_depth min_n .config               
+            <dbl>      <int> <int> <chr>                 
+1      0.00000001         10    18 Preprocessor1_Model022"
 
 final_trees <- finalize_model(
   trees_spec,
@@ -109,7 +109,7 @@ library(vip)
 
 final_trees %>%
   fit(CHECKUP_RULE_OUT_HIV ~ .,
-      data = trees_train
+      data = df_train
   ) %>%
   vip(geom = "point")
 
@@ -117,60 +117,47 @@ final_wf <- workflow() %>%
   add_recipe(trees_rec) %>%
   add_model(final_trees)
 
-final_res <- final_wf %>%
-  last_fit(trees_split,
-           metrics = metric_set(mn_log_loss,accuracy,roc_auc,specificity,sensitivity,j_index))
+#Fit train
+final_fit <- final_wf %>% fit(data = df_train)
+final_fit
 
-final_res %>%
-  collect_metrics()
+#Predict test
+df_test_wt <- glimpse(df_test) %>% select(-CHECKUP_RULE_OUT_HIV)
 
-final_res$.predictions
+predictions <- final_fit %>%
+  predict(new_data = df_test_wt, type = "prob")
+head(predictions)
 
-# OPTIMO THRESHOLD
-library(probably)
-threshold_data <- final_res$.predictions[[1]] %>%
-  threshold_perf(CHECKUP_RULE_OUT_HIV, .pred_1, thresholds = seq(0.1, 0.5, by = 0.0025))
-
-threshold_data %>%
-  filter(.threshold %in% c(0.1,0.2, 0.3, 0.4))
-
-#Rendimiento de la variable ´para encontrar el umbra óptimo
-library(ggplot2)
-
-threshold_data <- threshold_data %>%
-  filter(.metric != "distance") %>%
-  mutate(group = case_when(
-    .metric == "sens" | .metric == "spec" ~ "1",
-    TRUE ~ "2"
-  ))
-
-max_j_index_threshold <- threshold_data %>%
-  filter(.metric == "j_index") %>%
-  filter(.estimate == max(.estimate)) %>%
-  pull(.threshold)
-min(max_j_index_threshold)
-max(max_j_index_threshold)
-
-ggplot(threshold_data, aes(x = .threshold, y = .estimate, color = .metric, alpha = group)) +
-  geom_line() +
-  theme_minimal() +
-  scale_color_viridis_d(end = 0.9) +
-  scale_alpha_manual(values = c(.4, 1), guide = "none") +
-  geom_vline(xintercept = min(max_j_index_threshold), alpha = .6, color = "grey30") +#min(max_j_index_threshold)
-  labs(
-    x = "'Good' Threshold\n(above this value is considered 'good')",
-    y = "Metric Estimate",
-    title = "Balancing performance by varying the threshold",
-    subtitle = "Sensitivity or specificity alone might not be enough!\nVertical line = Max J-Index"
-  )
-
-#Best umbral
-best_umbral <- threshold_data %>%
-  filter(.threshold == min(max_j_index_threshold))
-
-
-predict_test <- ifelse(final_res$.predictions[[1]]$.pred_1>0.488,1,0)
-head(predict_test)
+predictions_class <- final_fit %>%
+  predict(new_data = df_test_wt)
+head(predictions_class)
 
 library(caret)
-confusionMatrix(as.factor(predict_test),as.factor(final_res$.predictions[[1]]$CHECKUP_RULE_OUT_HIV),positive = '1')
+confusionMatrix(predictions_class$.pred_class,as.factor(df_test$CHECKUP_RULE_OUT_HIV),positive = '1')
+"Confusion Matrix and Statistics
+
+          Reference
+Prediction     0     1
+         0 21806  2934
+         1   696   943
+                                          
+               Accuracy : 0.8624          
+                 95% CI : (0.8582, 0.8665)
+    No Information Rate : 0.853           
+    P-Value [Acc > NIR] : 7.697e-06       
+                                          
+                  Kappa : 0.2789          
+                                          
+ Mcnemar's Test P-Value : < 2.2e-16       
+                                          
+            Sensitivity : 0.24323         
+            Specificity : 0.96907         
+         Pos Pred Value : 0.57535         
+         Neg Pred Value : 0.88141         
+             Prevalence : 0.14697         
+         Detection Rate : 0.03575         
+   Detection Prevalence : 0.06213         
+      Balanced Accuracy : 0.60615         
+                                          
+       'Positive' Class : 1               
+                                    "
